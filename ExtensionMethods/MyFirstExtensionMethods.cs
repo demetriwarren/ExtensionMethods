2fa9bc99e5bcd6f667a22817945d108dca5bb4c4
Demetri Warren <dwarren@tql.com>
2024-06-19 15:19:12 -0400

    Add project files.

---
+++ ExtensionMethods/MyFirstExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods;
+                                                    // this is basically replacing the Console Commands
+public static class MyFirstExtensionMethods        
+{
+    public static void Debug(this string str, string label)   //void to say we will not return anything
+    {    
+        Console.WriteLine($"{label}: [{str}]");
+        
+    }
+    public static void Debug(this int i, string label)
+    {
+        Console.WriteLine($"{label}: [{i}]");
+    }
+    public static int Func(this int x)
+        {
+            return (x * x) + (x * 3) + 5;
+        }
+    public static int Cubed(this int x)
+    {
+        return x * x * x;
+    }
+    public static 
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExtensionMethods;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var str = "bruhhhhhh";
+        
+        str.Debug("str is");
+
+        var i = 12;
+        i.Debug("i is ");
+
+        i.Func().Debug($"f({i}) = ");
+
+        i.Cubed().Debug("I cubed");
+    }
+}
